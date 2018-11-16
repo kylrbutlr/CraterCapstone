57f938a13d4d46c54e7055b5a6306118de3ed362
unknown <brianvu@asu.edu>
2018-11-15 21:15:14 -0700

    PR fixes

--- Scripts/Spice.py
+++ Scripts/Spice.py
@@ -8,6 +8,7 @@ import spiceypy as spice
 import datetime as datetime
 import enum
 
+
 def compute_view_frustrum(instrument):
     """
     stub
@@ -34,24 +35,27 @@ Create Spice Wrappers Here:
 # Kernels #
 ###########
 
+
 class KernelType(enum.Enum):
     """
     Enum types for kernels
+    ** means kernels we will most likely be working with
 
     for more info about each type refer to:
     https://naif.jpl.nasa.gov/pub/naif/toolkit_docs/Tutorials/pdf/individual_docs/12_intro_to_kernels.pdf
     """
-    SPK = 'spk'     #**Spacecraft and Planet Ephemeris
-    PCK = 'pck'     #Planetary Constants
-    IK = 'ik'       #**Instrument
-    CK = 'ck'       #**Camera Matrix (Orientation)
-    EK = 'ek'       #Events
-    FK = 'fk'       #**Reference Frame Specification
-    SCLK = 'sclk'   #**Spacecraft Clock Correlation
-    LSK = 'lsk'     #**Leap Seconds (Time)
-    MK = 'mk'       #**Meta-Kernel (for loading many kernels)
-    DSK = 'dsk'     #Digital Shape Kernel
-    DBK = 'dbk'     #Database Mechanism
+    SPK = 'spk'     # **Spacecraft and Planet Ephemeris
+    PCK = 'pck'     # Planetary Constants
+    IK = 'ik'       # **Instrument
+    CK = 'ck'       # **Camera Matrix (Orientation)
+    EK = 'ek'       # Events
+    FK = 'fk'       # **Reference Frame Specification
+    SCLK = 'sclk'   # **Spacecraft Clock Correlation
+    LSK = 'lsk'     # **Leap Seconds (Time)
+    MK = 'mk'       # **Meta-Kernel (for loading many kernels)
+    DSK = 'dsk'     # Digital Shape Kernel
+    DBK = 'dbk'     # Database Mechanism
+
 
 def load_kernel(fileName : str):
     """
@@ -61,6 +65,7 @@ def load_kernel(fileName : str):
     """
     spice.furnsh(fileName)
 
+
 def unload_kernel(fileName : str):
     """
     Unload a single or multiple kernels (through meta-kernel file, .mk)
@@ -68,26 +73,29 @@ def unload_kernel(fileName : str):
     """
     spice.unload(fileName)
 
+
 def unload_all_kernels():
     """
     Unloads all kernels
     """
     spice.kclear()
 
-def check_number_kernels(kType : KernelType, value: int):
+
+def check_number_kernels(ktype : KernelType, value: int):
     """
     Check how many kernels are loaded for testing purposes
     :param kType: Kernel enum type
     :param value: amount of kernels that should be loaded
     :return: boolean if there is a correct amount of kernels loaded
     """
-    return spice.ktotal(kType.value) == value
+    return spice.ktotal(ktype.value) == value
 
 ########
 # Time #
 ########
 
-def convert_UTC_to_ET(date: datetime):
+
+def convert_utc_to_et(date: datetime):
     """
     Converts standard UTC date into ET (Ephemeris Time)
 
@@ -103,7 +111,8 @@ def convert_UTC_to_ET(date: datetime):
 # SPK Kernels #
 ###############
 
-def find_body_position(mainBody: str, etTime: float, referenceFrame: str, correction: str, observingBody: str):
+
+def find_body_position(main_body: str, time: float, reference_frame: str, correction: str, observing_body: str):
     """
     Loaded from SPK file
     Returns the position of a body relative to another observing body
@@ -111,18 +120,19 @@ def find_body_position(mainBody: str, etTime: float, referenceFrame: str, correc
     Example: body_position('Cassini', 595166468.1826606, 'J2000', 'NONE', 'SATURN BARYCENTER')
 
     https://naif.jpl.nasa.gov/pub/naif/toolkit_docs/C/cspice/spkpos_c.html
-    :param mainBody: the name of target body
-    :param Ephemeris Time: time in ET format (look at convert_UTC_to_ET)
-    :param referenceFrame: string referring to the reference frame of the output position vector
-    :param correction:  Abberation correction flag
-    :param observingBody: Name of observing body
-    :return: Position of target in a 3D vector
-    :return: light time between the objects as a double
+    :param main_body: the name of target body
+    :param time: Ephemeris Time of the observer's location
+    :param reference_frame: string referring to the reference frame of the output position vector
+    :param correction:  Aberration correction flag
+    :param observing_body: Name of observing body
+    :return: Position of target in a 3D vector,
+             Light time between the objects as a double
     """
 
-    return spice.spkpos(mainBody, etTime, referenceFrame, correction, observingBody)
+    return spice.spkpos(main_body, time, reference_frame, correction, observing_body)
 
-def find_frame_transformation(fromObject: str, toObject:str, etTime: float):
+
+def find_frame_transformation(from_object: str, to_object:str, time: float):
     """
     Returns the transformation matrix of how the frame is moving
 
@@ -130,44 +140,46 @@ def find_frame_transformation(fromObject: str, toObject:str, etTime: float):
 
     ftp://naif.jpl.nasa.gov/pub/naif/toolkit_docs/FORTRAN/spicelib/pxform.html
 
-    :param fromObject: name of FK of which we are coming from
-    :param toObject: name of FK of which we are going to
-    :param Ephemeris Time: Epoch of the rotation matrix
-    :return: ET is the amount the position has transformed (magnitude?)
-    :return: The 3x3 transformation matrix M, where (x,y,z)^T *M = (x2,y2,z2)^T
+    :param from_object: name of FK of which we are coming from
+    :param to_object: name of FK of which we are going to
+    :param time: Ephemeris Time Epoch of the rotation matrix
+    :return: ET is the amount the position has transformed (magnitude?),
+             The 3x3 transformation matrix M, where (x,y,z)^T *M = (x2,y2,z2)^T
     """
 
-    return spice.pxform(fromObject, toObject, etTime)
+    return spice.pxform(from_object, to_object, time)
 
 ##############
 # DSK Kernel #
 ##############
 
-def find_ray_surface_intercept(method: str, target: str, etTime: float, fixedReference:str, correction: str,
-                  observerName: str, directionReference:str, directionVector: list):
+
+def find_ray_surface_intercept(method: str, target: str, time: float, fixed_reference:str, correction: str,
+                  observer_name: str, direction_reference:str, direction_vector: list):
     """
-    Finds where a ray intercepts a surface
+    Finds where a ray intercepts a surface in Cartesian coordinates
 
     https://naif.jpl.nasa.gov/pub/naif/toolkit_docs/FORTRAN/spicelib/sincpt.html
 
-    :param method: computational method name such as 'ELLIPSOID' or 'DSK/UNPRIORITIZED[/SURFACES = <surface list>]'
+    :param method: computational method name such as 'DSK/UNPRIORITIZED[/SURFACES = <surface list>]' or 'ELLIPSOID'
     :param target: name of the target body
-    :param Ephemeris Time: et time of the frame
-    :param fixedReference: name of the FK that the target body is fixed to
+    :param time: Ephemeris Time of the frame
+    :param fixed_reference: name of the FK that the target body is fixed to
     :param correction: Aberration correction
-    :param observerName: the name of the observer taking the frame
-    :param directionReference: name of FK that is the reference frame of which the ray's direction is
-    :param directionVector: Ray direction vector that is from the observer
-    :return: The closest point in Cartesion coordinates
-    :return: The distance between the observer and the intercept point
-    :return: The observer's position at ET to the corrected position of surface intersect point
-    :return: Boolean value which expresses if the ray intersects
+    :param observer_name: the name of the observer taking the frame
+    :param direction_reference: name of FK that is the reference frame of which the ray's direction is
+    :param direction_vector: Ray direction vector that is from the observer
+    :return: The closest point,
+             The distance between the observer and the point,
+             The observer's position at ET to the corrected position of surface intersect point,
+             Boolean for if the ray intersects
     """
 
-    return spice.sincpt(method, target, etTime, fixedReference, correction, observerName, directionReference,
-                        directionVector)
+    return spice.sincpt(method, target, time, fixed_reference, correction, observer_name, direction_reference,
+                        direction_vector)
 
-def find_fov(instrumentID: int, maxReturn: int):
+
+def find_fov(instrument_id: int, max_return: int):
     """
     Return information about the instrument such as the shape, reference frame, direction of view, and the corner
     of the instruments. The shape can be a 'POLYGON', 'RECTANGLE', 'CIRCLE', or 'ELLIPSE'.
@@ -175,16 +187,17 @@ def find_fov(instrumentID: int, maxReturn: int):
     https://naif.jpl.nasa.gov/pub/naif/toolkit_docs/Tutorials/pdf/individual_docs/25_ik.pdf
     https://naif.jpl.nasa.gov/pub/naif/toolkit_docs/C/cspice/getfov_c.html
 
-    :param instrumentID: NAIF ID of the instrument (negative six digit number, example: -226801)
-    :param maxReturn: maximum number of vectors that you want to return in 'BOUNDS' (recommended: 4+)
-    :return: shape of the fov
-    :return: reference frame in which the field of view boundary vectors are defined
-    :return: Vector representing the view direction of the instrument
-    :return: the number of bounds returned
-    :return: 'BOUNDS' that points to the corners of the instrument
+    :param instrument_id: NAIF ID of the instrument (negative six digit number, example: -226801)
+    :param max_return: maximum number of vectors that you want to return in 'BOUNDS' (recommended: 4+)
+    :return:
+             Shape of the fov,
+             Reference frame in which the field of view boundary vectors are defined,
+             Vector representing the view direction of the instrument,
+             The number of 'BOUNDS' returned,
+             'BOUNDS' that points to the corners of the instrument
     """
 
-    return spice.getfov(instrumentID, maxReturn)
+    return spice.getfov(instrument_id, max_return)
 
 
 def print_ver():
@@ -200,7 +213,7 @@ if __name__ == '__main__':
     load_kernel('kernels/mk/ROS_OPS.TM')
     print('break')
     print(spice.ktotal(KernelType.SPK.value))
-    print(convert_UTC_to_ET(datetime.date.today()))
+    print(convert_utc_to_et(datetime.date.today()))
     frame, vector, number, bounds, bounds2 = find_fov(-226807, 4)
 
     print(bounds2)
